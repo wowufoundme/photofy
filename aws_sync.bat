1f477b0ceee0a7deb1dc513c7f7225569bed22cf
Shubhkirti Sharma <shubhkirti.work@gmail.com>
2022-04-20 14:26:56 -0400

    Update sync script to zip and update lambdas

--- aws_sync.bat
+++ aws_sync.bat
@@ -4,8 +4,19 @@
 @echo off
 cls
 set BUCKETNAME=photofy-app
-aws s3 sync . s3://%BUCKETNAME%
+echo ======================================================
+tar.exe -c -a -f .\artifacts\index_photos.zip index_photos.py
+tar.exe -c -a -f .\artifacts\search_photos.zip search_photos.py
+echo Syncing Lambda Function - Index Photos...
+aws lambda update-function-code --function-name index-photos --zip-file fileb://artifacts/index_photos.zip > .\dump\log_lf1_output.json
+echo Syncing Lambda Function - Search Photos...
+aws lambda update-function-code --function-name LF2-search-photos --zip-file fileb://artifacts/search_photos.zip > .\dump\log_lf2_output.json
+echo Lambda Sync Complete
+echo ======================================================
+echo Syncing AWS Bucket with resources
+aws s3 sync . s3://%BUCKETNAME% > .\dump\aws_sync_dump.txt
 echo ======================================================
 echo Sync complete with S3 bucket %BUCKETNAME%
 echo ======================================================
 pause
+cls
